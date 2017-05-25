bcae3bb0dbd7e4671cd339ae6767d8db46b6a14a
xuhaipeng <xuhaipeng@youbanban.com>
2017-05-25 15:55:08 +0800

    lazy

--- www/js/index.js
+++ www/js/index.js
@@ -2,5 +2,8 @@
  * Created by 30613 on 2017/5/23.
  */
 $(function(){
-    $('#resume').fullpage();
+    $('#resume').fullpage({
+        slidesNavigation: true,
+        loopHorizontal: true
+    });
 });
